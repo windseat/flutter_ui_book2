847a04b13fddff99944b8fb037bd19af5474ced1
Simon Castle <windseat@naver.com>
2023-05-07 12:22:04 +0900

    2023 05 07, p141 까지 따라함

---
+++ market_kurly_ui/lib/screens/home/components/kurly_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class KurlyPage extends StatelessWidget {
+  const KurlyPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
